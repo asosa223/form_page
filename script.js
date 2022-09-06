13bc76f32e2016d2097ea7192db83dbdde6c1cd9
Alec Sosa <alecsosa223@gmail.com>
2022-09-05 22:25:15 -0500

    Fonts, icon and JS updated

---
+++ script.js
@@ -0,0 +1,22 @@
+//FINAL TASKS
+//Add password validation 
+//return alert on form submit
+
+//const submit = document.querySelector(".submit");
+const form = document.querySelector("#bank-form")
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+
+form.addEventListener('submit', (e) => {
+    //prevents the form from submitting
+    e.preventDefault();
+})
+
+
+/*if (password.toString() === confirmPassword.toString()){
+    alert('password match');
+}*/
+
+/*submit.addEventListener('click', () => {
+    alert('Form Submitted');
+})*/
